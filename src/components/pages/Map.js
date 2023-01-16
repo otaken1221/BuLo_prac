ea1a61e2d251c2e333f131f3682c455341444096
Takeru Otsu <takeru.o.2002@gmail.com>
2023-01-16 23:17:21 +0900

    receive json

--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -1,10 +1,42 @@
-import { Dimensions, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Dimensions, StyleSheet, Text, View, SafeAreaView,  } from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import { URL, API_KEY } from '@env';
+import axios from 'axios';
+
+
+
+
+
+
+const url = URL;
+console.log(url);
+
+
 
 const Map = () => {
+  
+  axios.get(url, {headers: {AccessToken: API_KEY}})
+  .then((results) => {
+    console.log(results);
+  })
+  .catch((error) =>{
+  console.log(error);
+});
+
+  
   return (
    <SafeAreaView>
-    <MapView style={styles.map} provider={PROVIDER_GOOGLE}/>
+    <MapView 
+      style={styles.map}
+      provider={PROVIDER_GOOGLE}
+      initialRegion={{
+        latitude: 41.773481,
+        longitude: 140.727657,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      }}
+      />
    </SafeAreaView>
       
     
