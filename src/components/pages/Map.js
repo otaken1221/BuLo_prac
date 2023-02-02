4819c09be475dd8f95a5bdc0dc8ef2c681bc1a3f
Takeru Otsu <takeru.o.2002@gmail.com>
2023-02-03 00:05:04 +0900

    show location of muzirushi

--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -1,47 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import { Dimensions, StyleSheet, Text, View, SafeAreaView,  } from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import { URL, API_KEY } from '@env';
-import axios from 'axios';
-
-
-
-
-
+import React, { useState, useEffect } from "react";
+import { Dimensions, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { URL, API_KEY } from "@env";
+import axios from "axios";
 
 const url = URL;
-console.log(url);
-
-
 
 const Map = () => {
-  
-  axios.get(url, {headers: {AccessToken: API_KEY}})
-  .then((results) => {
-    console.log(results);
-  })
-  .catch((error) =>{
-  console.log(error);
-});
+  const [post, setPosts] = useState([]);
 
-  
+  useEffect(() => {
+    axios
+      .get(url, { headers: { AccessToken: API_KEY } })
+      .then((results) => {
+        setPosts(results.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
   return (
-   <SafeAreaView>
-    <MapView 
-      style={styles.map}
-      provider={PROVIDER_GOOGLE}
-      initialRegion={{
-        latitude: 41.773481,
-        longitude: 140.727657,
-        latitudeDelta: 0.05,
-        longitudeDelta: 0.05,
-      }}
-      />
-   </SafeAreaView>
-      
-    
+    <View>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 41.773481,
+          longitude: 140.727657,
+          latitudeDelta: 0.05,
+          longirtudeDelta: 0.05,
+        }}
+      >
+        {post.map((item) => {
+          return (
+            <Marker
+              pinColor={"red"}
+              key={item.id}
+              title={item.name}
+              coordinate={{
+                latitude: item.latitude,
+                longitude: item.longitude,
+              }}
+            >
+              
+            </Marker>
+          );
+        })}
+      </MapView>
+    </View>
   );
-}
+};
 
 export default Map;
 
